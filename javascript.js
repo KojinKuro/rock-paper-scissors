e726704bf7fb3ab8e117674f8da0f3e15450e721
Charles Kwang <11234292+KojinKuro@users.noreply.github.com>
2023-11-05 17:59:56 -0800

    Add scoring, looping, and win lose message
    The game keeps track of the score between each round. The game will loop
    fives times before spitting out who won or lost.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,31 @@
-//main gameloop runs once
-let playerChoice = prompt('Rock, paper, or scissors?','');
-let result = playRound(playerChoice, getComputerChoice());
-alert(result);
+//set the scores set to 0
+let playerScore = computerScore = 0;
+game();
+
+function game() {
+    const NUM_PLAYS = 5;
+    console.log('You are about to play 5 rounds of Rock Paper Scissors.')
+    for (let i = 1; i <= NUM_PLAYS; i++) {
+        //main game runs once
+        let playerChoice = prompt('Rock, paper, or scissors?','');
+        let computerChoice = getComputerChoice();
+        console.log(playRound(playerChoice, computerChoice));
+        console.log(`The player:computer score is ${playerScore}:${computerScore}`);
+    } console.log(checkWinner(playerScore,computerScore));
+}
+
+function checkWinner(score1,score2) {
+    let playerResult = (score1 > score2) ? 'won' : (score2 > score1) ? 'lost' : 'drew';
+    return `You ${playerResult} against the computer in RPS!`;
+}
+
+function addPlayerScore() {
+    playerScore += 1;
+}
+
+function addComputerScore() {
+    computerScore += 1;
+}
 
 function getComputerChoice() {
     //generate a number between 1 to 3
@@ -33,8 +57,12 @@ function playRound(playerSelection, computerSelection) {
     let gameResult = rpsSolver(playerSelection, computerSelection);
     if (gameResult === playerSelection) {
         gameText += `won! ${playerSelection} beats ${computerSelection}.`;
+        //add to playerScore
+        addPlayerScore();
     } else if (gameResult === computerSelection) {
         gameText += `lost! ${playerSelection} loses to ${computerSelection}.`;
+        //add to computerScore
+        addComputerScore();
     } else {
         // if you get errors in the code will default to a draw.
         gameText += `drew! ${playerSelection} and ${computerSelection} are the same.`;
@@ -42,34 +70,26 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function rpsSolver(res1, res2) {
+    //will return the value that wins. if its a draw or incalculable, returns nothing;
     switch (res1) {
         case 'Rock':
             if (res2 == 'Scissors') {
                 return res1;
             } else if (res2 == 'Paper') {
                 return res2;
-            } else {
-                return;
             } break;
         case 'Paper':
             if (res2 == 'Rock') {
                 return res1;
             } else if (res2 == 'Scissors') {
                 return res2;
-            } else {
-                return;
             } break;
         case 'Scissors':
             if (res2 == 'Paper') {
                 return res1;
             } else if (res2 == 'Rock') {
                 return res2;
-            } else {
-                return;
             } break;
-        default:
-            console.warn('rpsWinner() was not able to determine a winner');
-            return;
-            break;
-    }
+    // if cannot resolve will blank return
+    } return;
 }
